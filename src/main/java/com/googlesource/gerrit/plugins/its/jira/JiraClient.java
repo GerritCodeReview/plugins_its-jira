c877f2492b54f75492cf748ea9995d7ade7c9a68
Tobias Trobisch <tobiastrobisch@googlemail.com>
2016-02-11 14:05:42 +0100

    Switched from soap to rest api
    Change-Id: I20a05720a34ea4bb87c4be34f01d5dbff68ad82c

--- src/main/java/com/googlesource/gerrit/plugins/its/jira/JiraClient.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/jira/JiraClient.java
@@ -14,66 +14,133 @@
 
 package com.googlesource.gerrit.plugins.its.jira;
 
-import java.net.URL;
-import java.rmi.RemoteException;
+import com.atlassian.jira.rest.client.api.IssueRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
+import com.atlassian.jira.rest.client.api.RestClientException;
+import com.atlassian.jira.rest.client.api.domain.Comment;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.ServerInfo;
+import com.atlassian.jira.rest.client.api.domain.Transition;
+import com.atlassian.jira.rest.client.api.domain.input.TransitionInput;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.atlassian.util.concurrent.Promise;
+import com.googlesource.gerrit.plugins.its.base.its.InvalidTransitionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.atlassian.jira.rpc.soap.client.JiraSoapService;
-import com.atlassian.jira.rpc.soap.client.JiraSoapServiceServiceLocator;
-import com.atlassian.jira.rpc.soap.client.RemoteComment;
-import com.atlassian.jira.rpc.soap.client.RemoteFieldValue;
-import com.atlassian.jira.rpc.soap.client.RemoteIssue;
-import com.atlassian.jira.rpc.soap.client.RemoteNamedObject;
-import com.atlassian.jira.rpc.soap.client.RemoteServerInfo;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+/**
+ * Jira Rest Client.
+ */
 public class JiraClient {
+  private static final Logger log = LoggerFactory.getLogger(JiraClient.class);
 
-  private final JiraSoapService service;
+  private JiraRestClient client = null;
 
-  public JiraClient(final String baseUrl) throws RemoteException {
-    this(baseUrl, "/rpc/soap/jirasoapservice-v2");
+  /**
+   * @param url  jira url
+   * @param user username of the jira user
+   * @param pass password of the jira user
+   * @throws URISyntaxException
+   */
+  public JiraClient(String url, String user, String pass) throws URISyntaxException {
+    URI jiraUri = new URI(url);
+    log.debug("Trying to access Jira at " + jiraUri);
+    JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+    client = factory.createWithBasicHttpAuthentication(jiraUri, user, pass);
+    log.debug("Initialized jira client " + client);
   }
 
-  public JiraClient(final String baseUrl, final String rpcPath) throws RemoteException {
-    try {
-      JiraSoapServiceServiceLocator locator = new JiraSoapServiceServiceLocator();
-      service = locator.getJirasoapserviceV2(new URL(baseUrl+rpcPath));
+  /**
+   * @param issueKey Issue id
+   * @return Issueobjekt by issue id
+   * @throws RestClientException
+   */
+  public Issue getIssue(String issueKey) throws RestClientException {
+    IssueRestClient issueClient = this.client.getIssueClient();
+    return issueClient.getIssue(issueKey).claim();
+  }
+
+  /**
+   * @param issueKey Jira Issue key
+   * @return true if issue exists
+   */
+  public boolean issueExists(String issueKey) throws RestClientException {
+    boolean ret = true;
+    try{
+      getIssue(issueKey);
+    } catch (RestClientException e) {
+      if (e.getStatusCode().get() == 404){
+        log.error("Issue " + issueKey + " not found ");
+        ret = false;
+      } else {
+        throw e;
+      }
     }
-    catch (Exception e) {
-        throw new RemoteException("ServiceException during SOAPClient contruction", e);
+    return ret;
+  }
+
+  /**
+   * @param issueKey Jira Issue key
+   * @return Iterable of available transitions
+   * @throws RestClientException
+   */
+  public Iterable<Transition> getTransitions(String issueKey) throws RestClientException {
+    return client.getIssueClient().getTransitions(getIssue(issueKey)).claim();
+  }
+
+  /**
+   * @param issueKey Jira Issue key
+   * @param comment  Comment  to be added
+   * @throws RestClientException
+   */
+  public void addComment(String issueKey, Comment comment) throws RestClientException, URISyntaxException {
+    log.debug("Trying to add comment for issue " + issueKey);
+    Issue issue = getIssue(issueKey);
+    URI issueUri;
+    issueUri = new URI(issue.getSelf().toString() + "/comment/");
+    IssueRestClient issueClient = client.getIssueClient();
+    Promise<Void> promise = issueClient.addComment(issueUri, comment);
+    promise.claim();
+    log.debug("Comment added to issue " + issueKey);
+  }
+
+  /**
+   * @param issueKey   Jira Issue key
+   * @param transition Transition to perform
+   * @return true if successful
+   */
+  public boolean doTransition(String issueKey, String transition) throws RestClientException, InvalidTransitionException {
+    Transition t = getTransitionByName(getTransitions(issueKey), transition);
+    if (t == null) {
+      throw new InvalidTransitionException("Action " + transition
+        + " not executable on issue " + issueKey);
     }
+    TransitionInput input;
+    input = new TransitionInput(t.getId());
+    log.debug("Setting transition input to: " + input.toString());
+    client.getIssueClient().transition(getIssue(issueKey), input).claim();
+    return true;
   }
 
-  public JiraSession login(final String username, final String password) throws RemoteException {
-    String token = service.login(username, password);
-    return new JiraSession(username, token);
+  /**
+   * @return Serverinformation of jira
+   */
+  public ServerInfo sysInfo() throws RestClientException {
+    return client.getMetadataClient().getServerInfo().claim();
   }
 
-  public void logout(JiraSession token) throws RemoteException {
-    service.logout(getToken(token));
+  private Transition getTransitionByName(Iterable<Transition> transitions, String transition) {
+    Transition ret = null;
+    for (Transition t : transitions) {
+      if (transition.equals(t.getName())) {
+        ret = t;
+        break;
+      }
+    }
+    return ret;
   }
-
-  public RemoteIssue getIssue(JiraSession token, String issueKey) throws RemoteException {
-    return service.getIssue(getToken(token), issueKey);
-  }
-
-  public RemoteNamedObject[] getAvailableActions(JiraSession token, String issueKey) throws RemoteException {
-    return service.getAvailableActions(getToken(token), issueKey);
-  }
-
-  public RemoteIssue performAction(JiraSession token, String issueKey, String actionId, RemoteFieldValue... params) throws RemoteException {
-    return service.progressWorkflowAction(getToken(token), issueKey, actionId, params);
-  }
-
-  public void addComment(JiraSession token, String issueKey, RemoteComment comment) throws RemoteException {
-    service.addComment(getToken(token), issueKey, comment);
-  }
-
-  public RemoteServerInfo getServerInfo(JiraSession token) throws RemoteException {
-    return service.getServerInfo(getToken(token));
-  }
-
-  private String getToken(JiraSession token) {
-    return token == null ? null : token.getToken();
-  }
-
 }
