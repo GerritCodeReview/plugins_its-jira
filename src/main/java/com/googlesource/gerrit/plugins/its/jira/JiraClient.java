5e62eae48be6082d6b79e1d1762d93b2b5ee98f3
Remy Bohmer <linux@bohmer.net>
2017-03-18 21:19:30 +0000

    Replace Atlassian REST Library with simple REST calls
    The Atlassian library is a huge library containing a lot of
    functionality. It has dependencies to libraries that conflict
    with Gerrit Core libraries. It also makes it impossible to
    install the its-jira plugin multiple times.
    
    This change makes the implementation a whole lot simpler,
    and only uses the REST calls it actually needs.
    
    It also solves the issue on Gerrit Core where an exception
    is thrown every second in an endless loop which seems to pop
    up more frequent on Jira 7.2.7. This exception comes out of
    the Atlassian library:
    [2017-03-16 21:49:18,799] [httpclient-io:thread-4] ERROR org.apache.http.nio.protocol.HttpAsyncRequestExecutor : http-outgoing-254 [CLOSED] HTTP protocol exception: null
    java.nio.channels.ClosedChannelException
            at sun.nio.ch.SocketChannelImpl.ensureReadOpen(SocketChannelImpl.java:257)
            at sun.nio.ch.SocketChannelImpl.read(SocketChannelImpl.java:300)
            at org.apache.http.nio.reactor.ssl.SSLIOSession.receiveEncryptedData(SSLIOSession.java:340)
            at org.apache.http.nio.reactor.ssl.SSLIOSession.isAppInputReady(SSLIOSession.java:366)
            at org.apache.http.impl.nio.reactor.AbstractIODispatch.inputReady(AbstractIODispatch.java:118)
            at org.apache.http.impl.nio.reactor.BaseIOReactor.validate(BaseIOReactor.java:220)
            at org.apache.http.impl.nio.reactor.AbstractIOReactor.execute(AbstractIOReactor.java:284)
            at org.apache.http.impl.nio.reactor.BaseIOReactor.execute(BaseIOReactor.java:106)
            at org.apache.http.impl.nio.reactor.AbstractMultiworkerIOReactor$Worker.run(AbstractMultiworkerIOReactor.java:604)
            at java.lang.Thread.run(Thread.java:745)
    
    Change-Id: I50f66e7e0166fd2c26bfbad1ad29c76088371da9
    Signed-off-by: Remy Bohmer <linux@bohmer.net>

--- src/main/java/com/googlesource/gerrit/plugins/its/jira/JiraClient.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/jira/JiraClient.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2013 The Android Open Source Project
+// Copyright (C) 2013 - 2017 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,23 +14,23 @@
 
 package com.googlesource.gerrit.plugins.its.jira;
 
-import com.atlassian.jira.rest.client.api.IssueRestClient;
-import com.atlassian.jira.rest.client.api.JiraRestClient;
-import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
-import com.atlassian.jira.rest.client.api.RestClientException;
-import com.atlassian.jira.rest.client.api.domain.Comment;
-import com.atlassian.jira.rest.client.api.domain.Issue;
-import com.atlassian.jira.rest.client.api.domain.ServerInfo;
-import com.atlassian.jira.rest.client.api.domain.Transition;
-import com.atlassian.jira.rest.client.api.domain.input.TransitionInput;
-import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
-import com.atlassian.util.concurrent.Promise;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.googlesource.gerrit.plugins.its.base.its.InvalidTransitionException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * Jira Rest Client.
@@ -38,43 +38,38 @@ import java.net.URISyntaxException;
 public class JiraClient {
   private static final Logger log = LoggerFactory.getLogger(JiraClient.class);
 
-  private JiraRestClient client = null;
+  private final URL baseUrl;
+  private final String auth;
+  private final Gson gson;
 
   /**
    * @param url  jira url
    * @param user username of the jira user
    * @param pass password of the jira user
-   * @throws URISyntaxException
+   * @throws MalformedURLException
    */
-  public JiraClient(String url, String user, String pass) throws URISyntaxException {
-    URI jiraUri = new URI(url);
-    log.debug("Trying to access Jira at " + jiraUri);
-    JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
-    client = factory.createWithBasicHttpAuthentication(jiraUri, user, pass);
-    log.debug("Initialized jira client " + client);
-  }
-
-  /**
-   * @param issueKey Issue id
-   * @return Issueobjekt by issue id
-   * @throws RestClientException
-   */
-  public Issue getIssue(String issueKey) throws RestClientException {
-    IssueRestClient issueClient = this.client.getIssueClient();
-    return issueClient.getIssue(issueKey).claim();
+  public JiraClient(String url, String user, String pass) throws MalformedURLException {
+    String auth = user + ":" + pass;
+    this.auth = new String(Base64.encodeBase64(auth.getBytes()));
+    this.baseUrl = new URL(url);
+    this.gson = new Gson();
   }
 
   /**
    * @param issueKey Jira Issue key
    * @return true if issue exists
    */
-  public boolean issueExists(String issueKey) throws RestClientException {
+  public boolean issueExists(String issueKey) throws JiraRestException {
     boolean ret = true;
-    try{
-      getIssue(issueKey);
-    } catch (RestClientException e) {
-      if (e.getStatusCode().get() == 404){
+    try {
+      // This throws an exception if it does not exist: no exception means: issue exists
+      doGet("/rest/api/2/issue/" + issueKey, null);
+    } catch (JiraRestException e) {
+      if (e.getStatusCode().get() == HttpURLConnection.HTTP_NOT_FOUND) {
         log.error("Issue " + issueKey + " not found ");
+        ret = false;
+      } else if (e.getStatusCode().get() == HttpURLConnection.HTTP_FORBIDDEN) {
+        log.error("No permission to read Issue " + issueKey);
         ret = false;
       } else {
         throw e;
@@ -86,61 +81,128 @@ public class JiraClient {
   /**
    * @param issueKey Jira Issue key
    * @return Iterable of available transitions
-   * @throws RestClientException
+   * @throws JiraRestException
    */
-  public Iterable<Transition> getTransitions(String issueKey) throws RestClientException {
-    return client.getIssueClient().getTransitions(getIssue(issueKey)).claim();
+  public Iterable<JiraTransition.Item> getTransitions(String issueKey) throws JiraRestException {
+    JiraTransition t = (JiraTransition) doGet("/rest/api/2/issue/" + issueKey + "/transitions",
+      new TypeToken<JiraTransition>(){}.getType());
+    return Arrays.asList(t.transitions);
   }
 
   /**
    * @param issueKey Jira Issue key
-   * @param comment  Comment  to be added
-   * @throws RestClientException
+   * @param comment  String to be added
+   * @throws JiraRestException
    */
-  public void addComment(String issueKey, Comment comment) throws RestClientException, URISyntaxException {
+  public void addComment(String issueKey, String comment) throws JiraRestException {
+    if (!issueExists(issueKey)) {
+       log.error("Issue {} does not exist or no access permission", issueKey);
+       return;
+    }
     log.debug("Trying to add comment for issue " + issueKey);
-    Issue issue = getIssue(issueKey);
-    URI issueUri;
-    issueUri = new URI(issue.getSelf().toString() + "/comment/");
-    IssueRestClient issueClient = client.getIssueClient();
-    Promise<Void> promise = issueClient.addComment(issueUri, comment);
-    promise.claim();
+    doPost("/rest/api/2/issue/" + issueKey + "/comment", gson.toJson(new JiraComment(comment)));
     log.debug("Comment added to issue " + issueKey);
   }
 
   /**
    * @param issueKey   Jira Issue key
-   * @param transition Transition to perform
+   * @param transition JiraTransition.Item to perform
    * @return true if successful
    */
-  public boolean doTransition(String issueKey, String transition) throws RestClientException, InvalidTransitionException {
-    Transition t = getTransitionByName(getTransitions(issueKey), transition);
+  public boolean doTransition(String issueKey, String transition) throws JiraRestException, InvalidTransitionException {
+    log.debug("Making transition to {} for {}", transition, issueKey);
+    JiraTransition.Item t = getTransitionByName(getTransitions(issueKey), transition);
     if (t == null) {
       throw new InvalidTransitionException("Action " + transition
         + " not executable on issue " + issueKey);
     }
-    TransitionInput input;
-    input = new TransitionInput(t.getId());
-    log.debug("Setting transition input to: " + input.toString());
-    client.getIssueClient().transition(getIssue(issueKey), input).claim();
+    log.debug("Transition issue {} to '{}' ({})", issueKey, transition, t.getId());
+    doPost("/rest/api/2/issue/" + issueKey + "/transitions", gson.toJson(new JiraTransition(t)));
     return true;
   }
 
   /**
    * @return Serverinformation of jira
    */
-  public ServerInfo sysInfo() throws RestClientException {
-    return client.getMetadataClient().getServerInfo().claim();
+  public JiraServerInfo sysInfo() throws JiraRestException {
+    return (JiraServerInfo) doGet("/rest/api/2/serverInfo",
+      new TypeToken<JiraServerInfo>(){}.getType());
   }
 
-  private Transition getTransitionByName(Iterable<Transition> transitions, String transition) {
-    Transition ret = null;
-    for (Transition t : transitions) {
+  /**
+   * @return List of all projects we have access to in jira
+   */
+  public Iterable<JiraProject> getProjects() throws JiraRestException {
+    return (List<JiraProject>) doGet("/rest/api/2/project",
+      new TypeToken<List<JiraProject>>(){}.getType());
+  }
+
+  private JiraTransition.Item getTransitionByName(Iterable<JiraTransition.Item> transitions, String transition) {
+    JiraTransition.Item ret = null;
+    for (JiraTransition.Item t : transitions) {
       if (transition.equals(t.getName())) {
         ret = t;
         break;
       }
     }
     return ret;
+  }
+
+  /**
+   * Do a simple GET request.
+   */
+  private Object doGet(String spec, Type typeOfObject) throws JiraRestException {
+    try {
+      URL url = new URL(baseUrl, spec);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestProperty("Authorization", "Basic " + auth);
+      conn.setRequestMethod("GET");
+      conn.setRequestProperty("Accept", "application/json");
+
+      if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+        throw new JiraRestException("Get Request failed: " + url.toString(),
+          conn.getResponseCode());
+      }
+
+      Object object = null;
+      if (typeOfObject != null) {
+        InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+        object = gson.fromJson(isr, typeOfObject);
+        isr.close();
+      }
+      conn.disconnect();
+      return object;
+    } catch (IOException e) {
+      throw new JiraRestException(baseUrl + "/" + spec, e);
+    }
+  }
+
+  /**
+   * Do a simple POST request.
+   * We do not care about what data the server returns, pass/fail is based
+   * on HTTP-Response codes.
+   */
+  private void doPost(String spec, String jsonInput) throws JiraRestException {
+    try {
+      URL url = new URL(baseUrl, spec);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestProperty("Authorization", "Basic " + auth);
+      conn.setDoOutput(true);
+      conn.setRequestMethod("POST");
+      conn.setRequestProperty("Content-Type", "application/json");
+
+      OutputStream os = conn.getOutputStream();
+      os.write(jsonInput.getBytes());
+      os.flush();
+
+      if ((conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) &&
+        (conn.getResponseCode() != HttpURLConnection.HTTP_NO_CONTENT)) {
+        throw new JiraRestException("Post Request failed: " + url.toString() + " ->" + jsonInput,
+          conn.getResponseCode());
+      }
+      conn.disconnect();
+    } catch (IOException e) {
+      throw new JiraRestException(baseUrl + spec, e);
+    }
   }
 }
