1a728bef6a5c3b103b0cde28e37ac625e28ea0da
inderjot <inderjot.kaur.ratol@ericsson.com>
2017-11-22 15:48:55 -0500

    Support multiple Jira instances
    Currently, its-jira only supports connecting to an unique Jira server.
    In corporate environments, it is not unusual to have multiple Jira instances
    and is a common requirement to integrate Gerrit with those.
    
    This change enable its-jira to integrate with multiple Jira servers at
    the Gerrit project level i.e., a Gerrit project or group of projects
    can be associated with a Jira instance.
    
    Users can configure a Jira server for a project using GUI controls on
    the project's General page, specifying the Jira server url, username and
    password. The plugin takes care of creating a "commentlink" section in
    the project.config file accordingly. It is worth noting that the password
    will be encrypted right after being typed in the UI.
    
    A typical Jira server configuration in project.config will look like:
    
    [plugin "its-jira"]
      enabled = true
      url = http://localhost:8075/
      username = admin
      password = admin
    [commentlink "its-jira"]
      match = ([A-Z]+-[0-9]+)
      link = http://localhost:8075/browse/$1
    
    If its-jira plugin is enabled for a project and no Jira server is
    configured for this project, the default configuration will be the one
    defined in gerrit.config.
    
    The  plugin its-jira makes use of the ItsFacadeFactory to create in
    order to get the name of the project currently using its-jira.
    
    This change depends on an open change in its-base[1]
    which introduce the aforementioned ItsFacadeFactory interface.
    
    [1] https://gerrit-review.googlesource.com/c/plugins/its-base/+/148715/14
    
    Change-Id: I88f7298055ec1a136348758d4ec6fab67668e1ca

--- src/test/java/com/googlesource/gerrit/plugins/its/jira/JiraConfigTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/its/jira/JiraConfigTest.java
@@ -15,14 +15,14 @@
 package com.googlesource.gerrit.plugins.its.jira;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.googlesource.gerrit.plugins.its.jira.JiraConfig.GERRIT_CONFIG_PASSWORD;
-import static com.googlesource.gerrit.plugins.its.jira.JiraConfig.GERRIT_CONFIG_URL;
-import static com.googlesource.gerrit.plugins.its.jira.JiraConfig.GERRIT_CONFIG_USERNAME;
 import static org.mockito.Mockito.when;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.config.PluginConfig;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.project.NoSuchProjectException;
 import org.eclipse.jgit.lib.Config;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -33,38 +33,24 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class JiraConfigTest {
 
-  private static final String PASS = "pass";
-  private static final URL TEST_URL = newUrl("http://jira_example.com/");
-  private static final String USER = "user";
-  private static final String PLUGIN_NAME = "its-jira";
-
   @Rule public ExpectedException thrown = ExpectedException.none();
   @Mock private Config cfg;
+  @Mock private PluginConfigFactory cfgFactory;
 
+  private String pluginName = "its-jira";
   private JiraConfig jiraConfig;
 
-  @Test
-  public void gerritConfigContainsSaneValues() throws Exception {
-    when(cfg.getString(PLUGIN_NAME, null, GERRIT_CONFIG_URL)).thenReturn(TEST_URL.toString());
-    when(cfg.getString(PLUGIN_NAME, null, GERRIT_CONFIG_USERNAME)).thenReturn(USER);
-    when(cfg.getString(PLUGIN_NAME, null, GERRIT_CONFIG_PASSWORD)).thenReturn(PASS);
-    jiraConfig = new JiraConfig(cfg, PLUGIN_NAME);
-    assertThat(jiraConfig.getUsername()).isEqualTo(USER);
-    assertThat(jiraConfig.getPassword()).isEqualTo(PASS);
-    assertThat(jiraConfig.getJiraUrl()).isEqualTo(TEST_URL);
+  @Before
+  public void createJiraConfig() {
+    jiraConfig = new JiraConfig(cfg, pluginName, cfgFactory);
   }
 
   @Test
-  public void gerritConfigContainsNullValues() throws Exception {
-    thrown.expect(RuntimeException.class);
-    jiraConfig = new JiraConfig(cfg, PLUGIN_NAME);
-  }
-
-  private static URL newUrl(String url) {
-    try {
-      return new URL(url);
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
-    }
+  public void testGetPluginConfigFor() throws NoSuchProjectException {
+    Project.NameKey project = new Project.NameKey("$project");
+    PluginConfig pluginCfg = new PluginConfig(pluginName, new Config());
+    when(cfgFactory.getFromProjectConfigWithInheritance(project, pluginName)).thenReturn(pluginCfg);
+    jiraConfig.getPluginConfigFor(project);
+    assertThat(pluginCfg).isNotNull();
   }
 }
