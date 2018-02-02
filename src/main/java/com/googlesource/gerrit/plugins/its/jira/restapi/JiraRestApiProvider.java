2e0a83888a933e00054fc38c526a97a2f75ecc7f
Inderjot Kaur Ratol <inderjot.kaur.ratol@ericsson.com>
2018-02-01 22:26:08 -0500

    Fix Jira issue not found
    After I9a0183b58, plugin was able to connect to a Jira server but failed
    to find or annotate issues. The method URL(context,spec) in JiraRestApi
    caused the issue: If the spec begins with a forward slash, it replaces
    the path provided in the passed URL context as a first parameter.
    This is fixed by appending the spec to the URL string and converting it
    directly into the URL.
    
    Bug: Issue 8263
    Change-Id: I3ff701b25d9c69a4ebed88e3ff07f6390ea177a9

--- src/main/java/com/googlesource/gerrit/plugins/its/jira/restapi/JiraRestApiProvider.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/jira/restapi/JiraRestApiProvider.java
@@ -14,20 +14,19 @@ public class JiraRestApiProvider {
     this.pass = pass;
   }
 
-  public <T> JiraRestApi<T> get(Class<T> classOfT, String classPrefix)
-      throws MalformedURLException {
+  public <T> JiraRestApi<T> get(Class<T> classOfT, String classPrefix) {
     return new JiraRestApi<>(url, user, pass, classOfT, classPrefix);
   }
 
-  public JiraRestApi<JiraIssue> getIssue() throws MalformedURLException {
+  public JiraRestApi<JiraIssue> getIssue() {
     return get(JiraIssue.class, "/issue");
   }
 
-  public JiraRestApi<JiraServerInfo> getServerInfo() throws MalformedURLException {
+  public JiraRestApi<JiraServerInfo> getServerInfo() {
     return get(JiraServerInfo.class, "/serverInfo");
   }
 
-  public JiraRestApi<JiraProject[]> getProjects() throws MalformedURLException {
+  public JiraRestApi<JiraProject[]> getProjects() {
     return get(JiraProject[].class, "/project");
   }
 }
