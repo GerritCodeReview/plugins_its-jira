6267943f12e5515cd0ab6b25d61250eb2b321311
Inderjot Kaur Ratol <inderjot.kaur.ratol@ericsson.com>
2018-02-01 22:26:08 -0500

    Fix for the issue not found bug[1]
    The bug caused by the change[2].The method URL(context,spec) in
    JiraRestApi caused the issue. If the spec begins with a forward
    slash "/", it replaces the path provided in the already passed URL
    context as a first parameter. It is fixed by appending the spec to the
    URL string and converting it directly into the URL.
    
    [1] https://bugs.chromium.org/p/gerrit/issues/detail?id=8263
    [2] https://gerrit-review.googlesource.com/c/plugins/its-jira/+/144970
    
    Change-Id: I3ff701b25d9c69a4ebed88e3ff07f6390ea177a9

--- src/main/java/com/googlesource/gerrit/plugins/its/jira/restapi/JiraRestApi.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/jira/restapi/JiraRestApi.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.ProxySelector;
 import java.net.URL;
@@ -30,7 +29,7 @@ import org.eclipse.jgit.util.HttpSupport;
 /** Jira Rest Client. */
 public class JiraRestApi<T> {
   private static final String BASE_PREFIX = "rest/api/2";
-  private final URL baseUrl;
+  private final String baseUrl;
   private final String auth;
   private final Gson gson;
 
@@ -45,10 +44,9 @@ public class JiraRestApi<T> {
    * @param user username of the jira user
    * @param pass password of the jira user
    */
-  JiraRestApi(URL url, String user, String pass, Class<T> classOfT, String classPrefix)
-      throws MalformedURLException {
+  JiraRestApi(URL url, String user, String pass, Class<T> classOfT, String classPrefix) {
     this.auth = Base64.getEncoder().encodeToString((user + ":" + pass).getBytes());
-    this.baseUrl = new URL(url, BASE_PREFIX + classPrefix);
+    this.baseUrl = url + BASE_PREFIX + classPrefix;
     this.gson = new Gson();
     this.classOfT = classOfT;
   }
@@ -80,7 +78,7 @@ public class JiraRestApi<T> {
     return doGet(spec, passCode, null);
   }
 
-  URL getBaseUrl() {
+  String getBaseUrl() {
     return baseUrl;
   }
 
@@ -97,7 +95,8 @@ public class JiraRestApi<T> {
 
   private HttpURLConnection prepHttpConnection(String spec, boolean isPostRequest)
       throws IOException {
-    URL url = new URL(baseUrl, spec);
+    String urlWithSpec = baseUrl + spec;
+    URL url = new URL(urlWithSpec);
     ProxySelector proxySelector = ProxySelector.getDefault();
     Proxy proxy = HttpSupport.proxyFor(proxySelector, url);
     HttpURLConnection conn = (HttpURLConnection) url.openConnection(proxy);
