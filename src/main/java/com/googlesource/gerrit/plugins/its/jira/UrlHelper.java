a64a21f067242f3170df6244164aff41e0130427
Luca Milanesio <luca.milanesio@gmail.com>
2018-04-16 23:13:33 +0100

    Resolve URL syntax at init/config time
    Remove all the noise in the code about managing and adjusting the
    Jira URL and centralise validation in two points:
    - init
    - configuration
    
    Once the JiraConfig is resolved, all the rest of the code can assume
    that the URL is valid and can be used as-is.
    
    (cherry picked from commit: a87dd0e55f356731fc6ea7605f8471540ba505f0)
    Change-Id: I0ff634216c8df5b60a897e67ff84aad980b56791

---
+++ src/main/java/com/googlesource/gerrit/plugins/its/jira/UrlHelper.java
@@ -0,0 +1,50 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.its.jira;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UrlHelper {
+  private static final Logger log = LoggerFactory.getLogger(UrlHelper.class);
+
+  public static URL resolveUrl(URL url, String... paths) {
+    if (url == null) {
+      return url;
+    }
+
+    String relativePath = String.join("", Arrays.asList(paths));
+    try {
+      return new URL(url, relativePath);
+    } catch (MalformedURLException e) {
+      log.error("Unexpected exception while composing URL {} with path {}", url, relativePath, e);
+      throw new IllegalArgumentException(e);
+    }
+  }
+
+  public static URL adjustUrlPath(URL url) {
+    if (url == null) {
+      return url;
+    }
+    try {
+      return url.getPath().endsWith("/") ? url : new URL(url, "/");
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
