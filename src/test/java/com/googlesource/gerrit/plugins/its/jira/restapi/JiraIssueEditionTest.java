2193d3cc31a2a15d72c05aacb3ae62654e9e7c4b
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-06-03 17:40:35 +0200

    Add an event property value to an ITS field
    This change depends on https://gerrit-review.googlesource.com/c/plugins/its-base/+/181930.
    
    Vet-Version: 3.14
    Change-Id: Ibd93af414fe6f8e7de7d9651bdd21eade1b7b1e5

---
+++ src/test/java/com/googlesource/gerrit/plugins/its/jira/restapi/JiraIssueEditionTest.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.its.jira.restapi;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.server.OutputFormat;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonWriter;
+import java.io.StringWriter;
+import org.junit.Test;
+
+public class JiraIssueEditionTest {
+
+  private static final String FIELD_ID = "fieldId";
+  private static final String OPERATION = "operation";
+  private static final String VALUE = "value";
+
+  @Test
+  public void testSerialization() throws Exception {
+    JiraIssueEdition edition =
+        JiraIssueEdition.builder().appendUpdate(FIELD_ID, OPERATION, VALUE).build();
+
+    StringWriter stringWriter = new StringWriter();
+    new JsonWriter(stringWriter)
+        .beginObject()
+        .name("update")
+        .beginObject()
+        .name(FIELD_ID)
+        .beginArray()
+        .beginObject()
+        .name(OPERATION)
+        .value(VALUE)
+        .endObject()
+        .endArray()
+        .endObject()
+        .endObject()
+        .close();
+
+    assertThat(newGson().toJson(edition)).isEqualTo(stringWriter.toString());
+  }
+
+  private Gson newGson() {
+    return OutputFormat.JSON_COMPACT.newGson();
+  }
+}
