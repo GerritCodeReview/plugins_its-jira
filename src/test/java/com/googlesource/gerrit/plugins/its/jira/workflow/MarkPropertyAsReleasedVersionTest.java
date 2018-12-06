e3ff4d1c0f102ac2816778928f36488200c66cf3
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-12-06 15:05:56 +0100

    Mark property as released version
    This change depends on https://gerrit-review.googlesource.com/c/plugins/its-base/+/182592
    
    Change-Id: I3567e4200a162d2c6a47616fa93010eae440a480

---
+++ src/test/java/com/googlesource/gerrit/plugins/its/jira/workflow/MarkPropertyAsReleasedVersionTest.java
@@ -0,0 +1,64 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.its.jira.workflow;
+
+import static org.mockito.Mockito.*;
+
+import com.googlesource.gerrit.plugins.its.base.its.ItsFacade;
+import com.googlesource.gerrit.plugins.its.base.workflow.ActionRequest;
+import com.googlesource.gerrit.plugins.its.jira.JiraClient;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MarkPropertyAsReleasedVersionTest {
+
+  private static final String ITS_PROJECT = "test-project";
+  private static final String PROPERTY_ID = "propertyId";
+  private static final String PROPERTY_VALUE = "propertyValue";
+
+  private ItsFacade its;
+  private JiraClient jiraClient;
+  private MarkPropertyAsReleasedVersionParametersExtractor parametersExtractor;
+  private MarkPropertyAsReleasedVersion markPropertyAsReleasedVersion;
+
+  @Before
+  public void before() {
+    its = mock(ItsFacade.class);
+    jiraClient = mock(JiraClient.class);
+    parametersExtractor = mock(MarkPropertyAsReleasedVersionParametersExtractor.class);
+    markPropertyAsReleasedVersion =
+        new MarkPropertyAsReleasedVersion(jiraClient, parametersExtractor);
+  }
+
+  @Test
+  public void testHappyPath() throws IOException {
+    MarkPropertyAsReleasedVersionParameters extractedParameters =
+        mock(MarkPropertyAsReleasedVersionParameters.class);
+    when(extractedParameters.getPropertyValue()).thenReturn(PROPERTY_VALUE);
+
+    ActionRequest actionRequest = mock(ActionRequest.class);
+    Map<String, String> properties = Collections.singletonMap(PROPERTY_ID, PROPERTY_VALUE);
+    when(parametersExtractor.extract(actionRequest, properties))
+        .thenReturn(Optional.of(extractedParameters));
+
+    markPropertyAsReleasedVersion.execute(its, ITS_PROJECT, actionRequest, properties);
+
+    verify(jiraClient).markVersionAsReleased(ITS_PROJECT, PROPERTY_VALUE);
+  }
+}
