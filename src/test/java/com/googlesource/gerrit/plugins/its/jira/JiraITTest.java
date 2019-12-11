26cec7d71cd0adbc4db6c5f1fc20afc9e1ac3a17
David Ostrovsky <david@ostrovsky.org>
2019-12-11 12:45:48 -0800

    Adapt to Gerrit master
    GerritConfig class was moved to c.g.g.acceptance.config package in:
    I5050091e279.
    
    Change-Id: I99f7348841151c80712e3eb2903066df7871de94

--- src/test/java/com/googlesource/gerrit/plugins/its/jira/JiraITTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/its/jira/JiraITTest.java
@@ -28,7 +28,7 @@ import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import com.google.gerrit.acceptance.GerritConfig;
+import com.google.gerrit.acceptance.config.GerritConfig;
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.NoHttpd;
 import com.google.gerrit.acceptance.TestPlugin;
